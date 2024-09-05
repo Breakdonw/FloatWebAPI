d27e7b000b559e271392011873ffc211ddf9a4f5
Breakdonw <34593308+Breakdonw@users.noreply.github.com>
2024-09-04 22:38:43 -0400

    Added User auth controller and ORM db schema

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-console.log("Hello via Bun!");
+import express, {type Request, type Response } from 'express';
+import  Users  from './src/routes/Users';
+
+
+const app = express();
+const port = process.env.PORT || 5174; // 5173 is our UI
+const bodyparser = require('body-parser')
+const cors = require('cors');
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cors({ origin: true }));
+
+app.get('/',(req: Request, res: Response) =>{
+  res.send('You ventured to far.')
+})
+
+app.use('/User', Users)
+
+app.listen(port, ()=>{
+  console.log(`Server running @ port:${port}`)
+})
---
+++ src/routes/Users.ts
@@ -0,0 +1,139 @@
+import { Router,type Request,type Response } from "express";
+import * as Auth from "./components/authControllers";
+
+
+async function securityCheck(accessToken: any, res: Response, level?: number) {
+  
+  if (accessToken === undefined) {
+    res.status(500).send({ error: "No Token Provided" });
+    return false;
+  }
+
+  let token = await Auth.decodeToken(accessToken);
+  
+  let permissionLevel = level ? level : 1;
+  
+  if (!token || token === false) {
+    res.status(500).send({
+      error: "There was an error with your request. Please attempt to relogin.",
+    });
+    return false;
+  }
+
+  return token;
+}
+
+const router = Router();
+
+router.post("/Users/GetUsers", async (req: Request, res: Response) => {
+  if ((await securityCheck(req.body.accessToken, res, 3)) === false) {
+    return;
+  }
+  try {
+    let data = await Auth.default();
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(500).send({
+      error: "There was an error with your request. Please attempt to relogin.",
+    });
+  }
+
+  return;
+});
+
+router.post("/UpdateUser", async (req: Request, res: Response) => {
+  let token = await securityCheck(req.body.accessToken, res, 7);
+  let { body } = req;
+  try {
+   let result = await Auth.updateUser(
+      Number(body.userid),
+      body.firstName,
+      body.lastName,
+    );
+    result === true ?res.sendStatus(200) : res.sendStatus(500)
+  } catch (e) {
+    
+    console.warn(e);
+  }
+});
+
+
+
+
+router.post("/Login", async (req: Request, res: Response) => {
+  if (!req.body.email || !req.body.password) {
+    res.status(500).send({
+      error: "A required Field was missing when the login message was sent",
+    });
+    return 0;
+  }
+  try {
+    const data = await Auth.LoginUser(req.body.email, req.body.password, res);
+    console.log(await data?.token);
+    res.status(200).send({
+      message: "User logged in successfully",
+      accessToken: data?.token,
+      firstName: data?.fName,
+      lastName: data?.lName,
+      role: data?.role,
+    });
+  } catch (e) {
+    res.status(500).send({
+      error: " An error occured while processing this message ",
+      exception: `${e}`,
+    });
+    return 0;
+  }
+});
+
+router.post("/Register", async (req: Request, res: Response) => {
+  if (
+    !req.body.firstName ||
+    !req.body.email ||
+    !req.body.lastName ||
+    !req.body.password ||
+    !req.body.hid
+  ) {
+    res.status(500).send({
+      error: "A required Field was missing when the message was sent",
+    });
+    return 0;
+  }
+
+  try {
+    const token = await Auth.RegisterUser(
+      req.body.firstName,
+      req.body.lastName,
+      req.body.email,
+      req.body.password,
+      res
+    );
+    console.log(token);
+    res
+      .status(200)
+      .send({ message: "User Created succesfully!", accessToken: token });
+  } catch (e) {
+    res.status(500).send({
+      error: " An error occured while processing this message ",
+      exception: `${e}`,
+    });
+    return 0;
+  }
+});
+
+router.post("/VerifyToken", async (req: Request, res: Response) => {
+  if (!req.body.requiredRole || !req.body.accessToken) {
+    res.status(500).send({
+      error:
+        "A Token was not submitted was not submited thus could not be verified",
+    });
+    return 0;
+  }
+  if ((await Auth.verifyToken(req.body.accessToken, res)) === true) {
+    res.status(200).send({ message: "authorized" });
+  } else {
+    res.status(201).send({ error: "Token Expired" });
+  }
+});
+
+export default router;
